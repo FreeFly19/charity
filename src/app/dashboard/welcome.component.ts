fd4cd20d2dd4fd27a218a4a78214a2be539985b4
Mary <marysaphpir@gmail.com>
2017-10-01 23:15:22 +0300

    style dashboard

--- src/app/dashboard/welcome.component.ts
+++ src/app/dashboard/welcome.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'welcome',
-  template: '<h1 class="text-center">Welcome!</h1>',
+  template: ` <a href="/" id="logo">Choose option</a>
+                <ul id="menu">
+                  <li><a href="/"><span>Show products</span></a></li>
+                  <li><a href="/"><span>Show programs</span></a></li>
+                </ul>
+  `,
+  styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
 }
