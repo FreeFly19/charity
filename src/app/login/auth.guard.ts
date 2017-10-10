adf9f9a21282abf7fa353504aeb2e3ff5c5be5a0
Turch <TurchTas.1551@gmail.com>
2017-10-10 19:01:20 +0300

    fix something

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { UserService } from "../user.service";
+import { UserService } from '../user.service';
 
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
